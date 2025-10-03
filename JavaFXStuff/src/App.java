11fee95bd9447bf4d7b3017a19dae5dd8190f77f
Haowei-Oh <haoweioh123@email.tamu.edu>
2025-10-03 18:30:03 -0500

    Made example work

--- JavaFXStuff/src/App.java
+++ JavaFXStuff/src/App.java
@@ -1,11 +1,21 @@
 //import [library here]
-
-public class App {
-    public static void main(String[] args) throws Exception {
+public class App extends DatabaseApp {
+    public static void main(String[] args){
         System.out.println("Hello, World!");
         //Comments are like so!
         /*Multiline Comments! */
-        //System: OS related stuff
-        //
+        launch(); //Launches the screen
+        /*
+         * JavaFX Documentation!
+         * Source: https://www.tutorialspoint.com/javafx/javafx_architecture.htm
+         * using "javafx"
+         * animation -> animation stuff (decoration/transition stuff)
+         * application -> ?
+         * css -> Adds CSSlike stuff
+         * event -> handles events (like buttons presumably)
+         * geometry -> 2D Shapes
+         * stage -> top level container classes?
+         * scene -> 
+         */
     }
 }
---
+++ JavaFXStuff/src/DatabaseApp.java
@@ -0,0 +1,16 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class DatabaseApp extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("database-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setTitle("AWS PostgreSQL Query Example");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+}
