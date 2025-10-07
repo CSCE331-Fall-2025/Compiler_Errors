b011d6bc06b4289d74ffd1a0251fa6675769f4a1
jhineman-tamu <jachinema@tamu.edu>
2025-10-07 17:37:20 -0500

    Delete JavaFXStuff directory
    Replacing all files in directory in prep for new pull request

--- JavaFXStuff/src/App.java
+++
@@ -1,8 +0,0 @@
-//import [library here]
-public class App extends DatabaseApp {
-    public static void main(String[] args){
-        //Comments are like so!
-        /*Multiline Comments! */
-        launch(); //Launches the screen
-    }
-}
