2d0a18f919073abd904e089c07bb36f020c4f50c
jhineman-tamu <jachinema@tamu.edu>
2025-10-07 17:39:49 -0500

    Manager side (reupload for formatting)

---
+++ JavaFXStuff/src/App.java
@@ -0,0 +1,8 @@
+//import [library here]
+public class App extends DatabaseApp {
+    public static void main(String[] args){
+        //Comments are like so!
+        /*Multiline Comments! */
+        launch(); //Launches the screen
+    }
+}
