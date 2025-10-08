fa3b7671d7dbaff0bb8b7ca7a22186b00ab057db
jhineman-tamu <jachinema@tamu.edu>
2025-10-08 13:31:42 -0500

    Add files via upload

--- JavaFXStuff/src/App.java
+++ JavaFXStuff/src/App.java
@@ -7,14 +7,6 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class App extends Application {
-import java.util.Scanner;
-
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-public class App extends Application {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         DatabaseController dbContr = new DatabaseController();
@@ -47,53 +39,7 @@ public class App extends Application {
         launch(App.class, "--userType=" + userType); //Launches the program
         
         
-        Scanner scanner = new Scanner(System.in);
-        DatabaseController dbContr = new DatabaseController();
-        String username;
-        String password;
-        String userType;
-        //Tear the users of the database
-        dbContr.getUsers();
 
-        while(true)
-        {
-            System.out.print("Enter username: ");
-            username = scanner.nextLine();
-            System.out.print("Enter password: ");
-            password = scanner.nextLine();
-
-            userType = dbContr.auth(username,password);
-            if(userType.equals("CASHIER"))
-            {
-                System.out.println("Cashier Identified");
-                break;
-            }
-            else if(userType.equals("MANAGER"))
-            {
-                System.out.println("Manager Identified");
-                break;
-            }
-            System.out.println("Incorrect username or Password. Try Again");
-        }
-        launch(App.class, "--userType=" + userType); //Launches the program
-        
-        
-        //Comments are like so!
-        /*Multiline Comments! */
-
-
-        /*
-         * JavaFX Documentation!
-         * Source: https://www.tutorialspoint.com/javafx/javafx_architecture.htm
-         * using "javafx"
-         * animation -> animation stuff (decoration/transition stuff)
-         * application -> ?
-         * css -> Adds CSSlike stuff
-         * event -> handles events (like buttons presumably)
-         * geometry -> 2D Shapes
-         * stage -> top level container classes?
-         * scene -> 
-         */
     }
 
     //DatabaseController is where mappings between the database and the GUI take place
@@ -108,18 +54,17 @@ public class App extends Application {
         {
             //System.out.println("Cashier time!");
             //Load Cashier here
-            fxmlLoader = new FXMLLoader(getClass().getResource("database-view.fxml"));
+            fxmlLoader = new FXMLLoader(getClass().getResource("/CASHIER.fxml"));
         }
         else if(userType.equals("MANAGER"))
         {
             //System.out.println("Manager time!");
             //Load manager
-            fxmlLoader = new FXMLLoader(getClass().getResource("database-view.fxml"));
+            fxmlLoader = new FXMLLoader(getClass().getResource("/MANAGER.fxml"));
         }
         else
         {
-            //Else, load default temporary fxml
-            fxmlLoader = new FXMLLoader(getClass().getResource("database-view.fxml"));
+            System.exit(0);
         }
         Scene scene = new Scene(fxmlLoader.load(), 600, 400); //What fxml, X-Size, Y-Size
         stage.setTitle("AWS PostgreSQL Query Example"); //Name of the Application
